b6a30ed76e60f176a760859b091123f1a9849bcd
afrodayt <afrodayt@gmail.com>
2024-11-01 09:05:02 +0100

    feat: VUE 2 + bootstrap + less

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+import Vue from 'vue/dist/vue.common.dev.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+
+import ExampleComponent from '@/components/ExampleComponent.vue';
+
+new Vue({
+    el: '#app',
+    components: {
+        ExampleComponent,
+    },
+});
